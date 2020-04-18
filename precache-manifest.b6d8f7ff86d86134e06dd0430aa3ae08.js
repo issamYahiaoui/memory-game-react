71d199af2903a743c50d8c7584e8af189a9b9979
issamYahiaoui <ei_yahiaoui@esi.dz>
2020-04-18 11:11:04 +0100

    Updates

---
+++ precache-manifest.b6d8f7ff86d86134e06dd0430aa3ae08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c569bb28f90e7e9348d8ec98e703b7",
+    "url": "/memory-game-react/index.html"
+  },
+  {
+    "revision": "9b6ec45d916e9cf8dd13",
+    "url": "/memory-game-react/static/css/main.775358ad.chunk.css"
+  },
+  {
+    "revision": "7d7b91957cbed9087856",
+    "url": "/memory-game-react/static/js/2.80dd0dea.chunk.js"
+  },
+  {
+    "revision": "ab4f240372d70b549005d11aae96d78c",
+    "url": "/memory-game-react/static/js/2.80dd0dea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b6ec45d916e9cf8dd13",
+    "url": "/memory-game-react/static/js/main.e70f3339.chunk.js"
+  },
+  {
+    "revision": "de2ed0cffc7c0ad4f44a",
+    "url": "/memory-game-react/static/js/runtime-main.03ee08ef.js"
+  }
+]);
